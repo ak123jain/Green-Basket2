1cfa0124daf84c2e2e66050fbcefb08fc31d8d7b
AkASH jain <aj9882854@gmail.com>
2024-12-09 22:17:53 +0530

    project setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/',()=>{
+    res.send("server is redy")
+})
+
+
+const port = process.env.PORT || 3000
+
+app.listen(port , ()=>{
+    console.log(`server is listenning at port http:localhost:${port}`);
+    
+})
