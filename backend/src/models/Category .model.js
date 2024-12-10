06b78bed69ddd826eb28b644d57514ee891e6ba3
AkASH jain <aj9882854@gmail.com>
2024-12-10 19:59:27 +0530

    category model

--- backend/src/models/Category .model.js
+++ backend/src/models/Category .model.js
@@ -1,16 +1,23 @@
 import mongoose , {Schema} from "mongoose";
 
 const CategorySchema = new Schema({
+
     name : {
         type : String,
         required : true,
         unique : true
     },
+
     description :{
         type : String,
         required : true
     },
-    
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+
 },{timestamps : true})
 
 export const Category = mongoose.model("Category" , CategorySchema)
