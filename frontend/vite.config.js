9e72abd8d055a92bba138396c5e56120ff57651f
AkASH jain <aj9882854@gmail.com>
2025-03-12 16:24:52 +0530

    build in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,4 +13,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "Gren-Basket2",  // Replace with your GitHub repository name
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure Vite outputs to `dist`
+  }
 })
