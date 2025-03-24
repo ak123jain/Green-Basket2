53aaec933276cc86455830df9569edd2ffa43b24
AkASH jain <aj9882854@gmail.com>
2025-03-24 18:46:07 +0530

    remove

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
  
 import { addproduct, getproduct } from "../controller/Product.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
-import { payment } from "../controller/Payment.controller.js";
+ 
 
  const router = Router()
 
