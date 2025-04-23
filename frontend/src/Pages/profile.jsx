b392bc7db96f94e9c1d7aa360f3c9add50c4aff5
AkASH jain <aj9882854@gmail.com>
2025-04-23 23:57:55 +0530

    seee

--- frontend/src/Pages/profile.jsx
+++ frontend/src/Pages/profile.jsx
@@ -34,7 +34,7 @@ const Profile = () => {
 
     if (loading) {
         return (
-            <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+            <div className="min-h-screen bg-gray-50 flex items-center justify-center mt-13">
                 <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
                 <p className="ml-3 text-lg font-medium text-gray-700">Loading...</p>
             </div>
